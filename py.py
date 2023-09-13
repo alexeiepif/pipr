a111cd415486b3b6c38933563d9930d61be4631d
alexeiepif <aled2093746@gmail.com>
2023-09-13 21:30:43 +0300

    final version

--- py.py
+++ py.py
@@ -5,8 +5,8 @@ for i in range(10):
     print(i, "\t", end="")
 print()
 for i in range(10):
-    print(i**2, "\t" , end="")
+    print(i**2, "\t", end="")
 print()
 for i in range(10):
-    print(i**3, "\t", end = "")
+    print(i**3, "\t", end="")
 print()
