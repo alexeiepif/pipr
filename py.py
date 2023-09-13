5e9b8870d18946ec6256da7f9586628ef617360a
alexeiepif <aled2093746@gmail.com>
2023-09-13 21:17:18 +0300

    new version

--- py.py
+++ py.py
@@ -2,5 +2,7 @@ print("hello ivt")
 print("ALEXEI EPIFANOV")
 for i in range(10):
     print(i, " ", end="")
+for i in range(10):
     print(i**2, " " , end="")
+for i in range(10):
     print(i**3, " ", end = "")
